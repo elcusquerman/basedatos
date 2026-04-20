57b8941a5b32da9339cacf758ac2cdd7da607a21
FACULTADES\alumnoFI <alumnoFI>
2026-04-20 09:21:37 -0300

    auto-generated commit message

--- v1/index.js
+++ v1/index.js
@@ -6,6 +6,7 @@ import usuariosRouter from "./routes/usuarios.routes.js";
 import dragonesRouter from "./routes/dragones.routes.js";
 import tesorosRouter from "./routes/tesoros.routes.js";
 import {authJWTMiddleware} from "./middlewares/authJWT.middleware.js";
+import aiRouter from "./routes/ai.routes.js";
 const router = express.Router({ mergeParams:true });
 
 //rutas no protegidas
@@ -14,6 +15,7 @@ router.use("/productos", productosRouter);
 router.use("/usuarios", usuariosRouter);
 router.use("/dragones", dragonesRouter);
 router.use("/tesoros", tesorosRouter);
+router.use("/ai", aiRouter);
 
 router.use(authJWTMiddleware);
 router.use("/mascotas", mascotasRouter);
